503c3e2fa621b6e53b63971c9e3b3c51d54beddb
dinesvlarsen <dinesvlarsen@gmail.com>
2021-10-01 14:46:48 +0200

    added style and designs, implemented the countdown code and hooked it up to the dom

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+'use strict';
+const counterDom = document.querySelector('#countdown');
+
+// console.log(counter);
+function reformatTime(time) {
+	return String(time).padStart(2, 0);
+}
+
+function pasteTimerToDom(string) {
+	counterDom.innerHTML = `<p>${string}</p>`;
+}
+
+function countDown() {
+	// const presentDate =
+	// const futureDate = new Date('2021-10-30T12:59:00.501Z').getTime();
+	let timeToConvert = Math.trunc(
+		new Date('2021-10-30T12:59:00.501Z').getTime() - new Date().getTime()
+	);
+	let time = new Date(timeToConvert);
+
+	const day = time.getDate();
+	const hour = time.getHours();
+	const min = time.getMinutes();
+	const sec = time.getSeconds();
+
+	let countDownString = `${reformatTime(day)} : ${reformatTime(
+		hour
+	)} : ${reformatTime(min)} : ${reformatTime(sec)}`;
+	pasteTimerToDom(countDownString);
+	timeToConvert -= 1000;
+}
+
+setInterval(countDown, 1000);
