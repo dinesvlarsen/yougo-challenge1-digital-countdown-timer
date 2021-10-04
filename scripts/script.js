7677cbb0884debdd3e567ad57fa8352412092d4f
dinesvlarsen <dinesvlarsen@gmail.com>
2021-10-04 08:28:57 +0200

    refactored some of the code, and removed some logic that didnt do anything

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
 'use strict';
 const counterDom = document.querySelector('#countdown');
 
+function getTime() {
+	let time = new Date(renderTime());
+	const day = time.getDate();
+	const hour = time.getHours();
+	const min = time.getMinutes();
+	const sec = time.getSeconds();
+	return [day, hour, min, sec];
+}
+
 function reformatTime(time) {
 	return String(time).padStart(2, 0);
 }
@@ -9,22 +18,31 @@ function pasteTimerToDom(string) {
 	counterDom.innerHTML = `<p>${string}</p>`;
 }
 
-function countDown() {
-	let timeToConvert = Math.trunc(
-		new Date('2021-10-30T12:59:00.501Z').getTime() - new Date().getTime()
+function renderTime() {
+	return Math.trunc(
+		new Date('2021-10-30T00:01:00.501Z').getTime() - new Date().getTime()
 	);
-	let time = new Date(timeToConvert);
-
-	const day = time.getDate();
-	const hour = time.getHours();
-	const min = time.getMinutes();
-	const sec = time.getSeconds();
-
-	let countDownString = `${reformatTime(day)} : ${reformatTime(
-		hour
-	)} : ${reformatTime(min)} : ${reformatTime(sec)}`;
-	pasteTimerToDom(countDownString);
-	timeToConvert -= 1000;
 }
+
+function countdownStringCreator(day, hour, min, sec) {
+	return `${reformatTime(day)} : ${reformatTime(hour)} : ${reformatTime(
+		min
+	)} : ${reformatTime(sec)}`;
+}
+
+function countDown() {
+	pasteTimerToDom(
+		countdownStringCreator(
+			getTime()[0],
+			getTime()[1],
+			getTime()[2],
+			getTime()[3]
+		)
+	);
+}
+
+pasteTimerToDom(
+	countdownStringCreator(getTime()[0], getTime()[1], getTime()[2], getTime()[3])
+);
 
 setInterval(countDown, 1000);
